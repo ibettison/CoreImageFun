d781213a3a52c963cab3f51ff241acd006911b87
Ian Bettison <ian.bettison@ncl.ac.uk>
2014-05-16 16:02:05 +0100

    Working Copy commits

--- CoreImageFun/ViewController.h
+++ CoreImageFun/ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Ray Wenderlich on 9/20/12.
 //  Copyright (c) 2012 Razeware LLC. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- CoreImageFun/ViewController.m
+++ CoreImageFun/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Ray Wenderlich on 9/20/12.
 //  Copyright (c) 2012 Razeware LLC. All rights reserved.
-//
+
 
 #import "ViewController.h"
 #import <AssetsLibrary/AssetsLibrary.h>
